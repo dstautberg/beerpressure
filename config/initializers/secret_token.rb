5f4e313b7e4f98977f3df84e85eec02428969466
Gayle <gayle@craigworld.com>
2011-11-11 23:41:52 -0500

    initial generated rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beerpressure::Application.config.secret_token = '77c5af4461e18ee4a578a120f65eb2d02992f0aa8ed6f56fc676e0c1ec33f1ffe8ff0619f96a2c1e32dbb56c937d8985430a4c0d256ecae33502585ad7ba9987'
