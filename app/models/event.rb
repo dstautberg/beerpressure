9525fb40c0605a948b4950cff91e1678d33a5036
Dean Stautberg <dean@stautberg.net>
2011-12-18 19:22:27 -0500

    Added an invitees text area to the new event page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ActiveRecord::Base
-  belongs_to :creator, :class_name => "User"
-  # has_many :invitees
+	belongs_to :creator, :class_name => "User"
+	has_many :invitees
+
+	# Assign invitees from a comma or space delimited string
+	def invitee_string
+	
+	end
 end
